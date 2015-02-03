e76b2aa95049e63da9f0946e051a64fe1aec1f14
Jay Xu <sgliv8@gmail.com>
2015-02-03 05:36:27 -0800

    Setup paperclip for image upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation) }
+
+    devise_parameter_sanitizer.for(:account_update) << :first_name
+    devise_parameter_sanitizer.for(:account_update) << :last_name
+    devise_parameter_sanitizer.for(:account_update) << :profile_name
   end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
 
 	belongs_to :user
+
+	has_attached_file :image, styles: { medium: "700x500#", thumb: "350x250>" }
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
