2e6a5949ad2c5a3cc2e421210103402fdc5b3509
Jay Xu <sgliv8@gmail.com>
2015-02-03 06:04:08 -0800

    Added comments to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
+  has_many :comments
 
   def full_name
   	first_name + " " + last_name
