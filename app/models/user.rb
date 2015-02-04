c7f46b6fec01b755153bba6a8c8e5a2bd8fd0383
Jay Xu <sgliv8@gmail.com>
2015-02-04 05:23:09 -0800

    Added gravatar for user profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
   def full_name
   	first_name + " " + last_name
   end
+
+  def gravatar_url
+  	hash = Digest::MD5.hexdigest(email.strip.downcase)
+
+  	"http://www.gravatar.com/avatar/#{hash}"
+  end
 end
